b83e5980a3c2880db6f6ecb191e4dca1b5e1b839
Dmitry Grigoriev <diman03051995@mail.ru>
2024-01-27 15:50:02 +0300

    the fifth step of the project

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,74 +1,73 @@
-#!/usr/bin/env python3
-import prompt
 from random import randint
+import prompt
 
 
-def welcome_user():   # Приветствие пользователя
+"""
+Описание игры "Проверка на чётность":
+
+Суть игры состоит в следующем: пользователю будет показано случайное число,
+и ему нужно будет определить, является ли это число чётным или нечётным.
+Пользователю будет предложено ввести "yes", если число чётное,
+или "no", если число нечётное.
+
+Если пользователь даст неверный ответ, игра будет завершена,
+и ему будет выведено сообщение:
+"'yes' is the wrong answer ;(. The correct answer was 'no'.
+Let's try again, (имя пользователя)!"
+
+Любой некорректный ввод также будет считаться ошибкой,
+например, введение "n" будет равносильно неправильному ответу.
+
+В случае, если пользователь введёт верный ответ, игра будет продолжена,
+и ему будет выведено сообщение "Correct!".
+
+Для успешного прохождения игры пользователю необходимо
+правильно ответить на три вопроса подряд.
+
+После успешного завершения игры будет выведено сообщение
+"Congratulations, (имя пользователя)".
+
+Здесь будет следующая часть кода для реализации игры...
+"""
+
+
+def welcome_user() -> str:   # Приветствие пользователя
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name.capitalize()}!')
-    return name.capitalize()
+    name_user = prompt.string('May I have your name? ')
+    print(f'Hello, {name_user.capitalize()}!')
+    return name_user.capitalize()
 
 
-name_user = ''  # Имя пользователя
-
-
-def game_instructions():  # Инструкция для пользователя
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
-def even_number(number):  # Проверка: четное или нечетное число
-    even = ''
+# Возвращает число и проверяет на чётность
+def _parity_check() -> tuple[str, str]:
+    number = randint(0, 100)
     if number % 2 == 0:
-        even = 'yes'
-    else:
-        even = 'no'
-    return even
+        return str(number), 'yes'
+    return str(number), 'no'
 
 
-def user_answer():  # Ответ пользователя
+# Проверка корректного ответа пользователя
+def correct_answer(name_user: str, correct: str) -> bool:
     answer = input('Your answer: ')
-    return answer
-
-
-correct = 'Correct!'   # Переменная для сравнения правильного ответа
-
-
-def check_answer(reply, chance):  # Проверка корректного ответа пользователя
-    check = ''
-    if reply == 'yes' and chance == 'yes':
-        check = correct
-    elif reply == 'no' and chance == 'no':
-        check = correct
+    if answer == correct:
+        return True
     else:
-        check = f"'{reply}' is wrong answer ;(. Correct answer was 'no'.\n" \
-                f"Let's try again, {name_user}!"
-    return check
+        print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\n"
+              f"Let's try again, {name_user}!")
 
 
-def game():
-    count = 0
-    while count < 3:
-        random_number = randint(1, 100)
-        print('Question: ', random_number)
-        response_user = user_answer()
-        if check_answer(response_user, even_number(random_number)) == correct:
-            count += 1
-            if count == 3:
-                print(f'Congratulations, {name_user}!')
-        else:
-            print(f"'{response_user}' is wrong answer ;(."
-                  f"Correct answer was 'no'.\nLet's try again, {name_user}!")
-            break
-
-
+# Функционал игры
 def main():
-    global name_user
-    global correct
     name_user = welcome_user()
-    game_instructions()
-    game()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    count_answer = 0
+    while count_answer < 3:
+        parity_check = _parity_check()
+        print(f'Question: {parity_check[0]}')
+        if not correct_answer(name_user, parity_check[1]):
+            return
 
+        print('Correct!')
+        count_answer += 1
 
-if __name__ == '_main_':
-    main()
+    print(f'Congratulations, {name_user}!')
